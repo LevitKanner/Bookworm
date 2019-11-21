d543633d6da3594dcc3717a7811550ff31f378e9
Levit Osei Kanner <lkanner21@gmail.com>
2019-11-21 09:43:37 +0000

    Creates a rating widget

--- Bookworm/RatingView.swift
+++ Bookworm/RatingView.swift
@@ -9,13 +9,42 @@
 import SwiftUI
 
 struct RatingView: View {
+    @Binding var rating: Int
+    var maximumRating = 5
+    var label = ""
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            if self.label.isEmpty == false {
+                Text(label)
+            }
+            
+            ForEach(1..<maximumRating + 1){ number in
+                self.image(for: number)
+                    .foregroundColor(number > self.maximumRating ? self.offColor : self.onColor)
+                    .onTapGesture {
+                        self.rating = number 
+                }
+                
+            }
+        }
+    }
+    
+    func image(for number: Int) -> Image{
+        if number > rating {
+            return offImage ?? onImage
+        }else {
+            return onImage
+        }
     }
 }
 
 struct RatingView_Previews: PreviewProvider {
     static var previews: some View {
-        RatingView()
+        RatingView(rating: .constant(4))
     }
 }
